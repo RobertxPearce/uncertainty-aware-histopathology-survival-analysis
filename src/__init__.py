6f5e2ffb8ca7d86309e66880114ca05805729bfb
RobertxPearce <robertbdpearce@gmail.com>
2026-07-06 13:46:52 -0700

    Update public API for easy importing

---
+++ src/__init__.py
@@ -0,0 +1,92 @@
+# Public API for the histopathology survival-analysis package.
+#
+#   load_survival_table  ->  make_datasets  ->  make_dataloaders     (data)
+#   build_model / build_optimizer                                    (model)
+#   train                                                            (training)
+#   predict / evaluate                                               (prediction & evaluation)
+#   mc_dropout_predict / deep_ensemble_predict                       (uncertainty)
+
+from .data import (
+    make_survival_metadata,
+    attach_feature_paths,
+    make_splits,
+    load_survival_table,
+    make_datasets,
+    make_dataloaders,
+    make_dataloaders_from_csv,
+    SurvivalBagDataset,
+    collate_bags,
+)
+from .models import (
+    ABMILSurvival,
+    build_model,
+)
+from .losses import (
+    cox_loss,
+)
+from .train import (
+    build_optimizer,
+    train_one_epoch,
+    evaluate_epoch,
+    train,
+)
+from .eval import (
+    concordance_index,
+    predict,
+    evaluate,
+    bootstrap_cindex,
+    mc_dropout_predict,
+    deep_ensemble_predict,
+)
+from .utils import (
+    seed_everything,
+    worker_init_fn,
+    make_generator,
+    pick_device,
+    save_checkpoint,
+    load_checkpoint,
+)
+
+__all__ = [
+    # data loading -> datasets -> dataloaders
+    "make_survival_metadata",
+    "attach_feature_paths",
+    "make_splits",
+    "load_survival_table",
+    "make_datasets",
+    "make_dataloaders",
+    "make_dataloaders_from_csv",
+    "SurvivalBagDataset",
+    "collate_bags",
+
+    # model construction
+    "ABMILSurvival",
+    "build_model",
+    "build_optimizer",
+
+    # loss
+    "cox_loss",
+
+    # training
+    "train",
+    "train_one_epoch",
+    "evaluate_epoch",
+
+    # prediction and evaluation
+    "predict",
+    "evaluate",
+    "concordance_index",
+    "bootstrap_cindex",
+
+    # uncertainty estimation
+    "mc_dropout_predict",
+    "deep_ensemble_predict",
+
+    # utilities
+    "seed_everything",
+    "worker_init_fn",
+    "make_generator",
+    "pick_device",
+    "save_checkpoint",
+    "load_checkpoint",
+]
